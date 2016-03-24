8fd0bba15fe3001fb60f9d00be2788f845efc3dd
andrueastman <andrueastman@gmail.com>
2016-03-24 22:21:54 +0300

    Cant see my contours for some reason. Will need to find a solution to that to knowif i am correct :(

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,11 +22,24 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+lambda_vec = [0.01 0.03 0.1 0.3 1 3 10 30]';
+error=1;
+x=1;yi=1;
+for i = 1:length(lambda_vec)
+    for j = 1:length(lambda_vec)
+        model= svmTrain(X, y, i, @(x1, x2) gaussianKernel(x1, x2, j));
+        predictions = svmPredict(model, Xval);
+        min=mean(double(predictions ~= yval));
+        if min<error
+            error=min
+            x=i;
+            yi=j;
+        end
+    end
+end
 
-
-
-
-
+C= lambda_vec(x);
+sigma = lambda_vec(yi);
 
 
 % =========================================================================
